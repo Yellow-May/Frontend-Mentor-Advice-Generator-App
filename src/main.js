bddcd3e9c3db3cc887764376eed4983476a287a0
yellow_may <yellow.may.21@gmail.com>
2022-06-15 12:27:39 +0100

    UI init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import MyApp from './app.vue';
 
-createApp(App).mount('#app')
+createApp(MyApp).mount('#app');
